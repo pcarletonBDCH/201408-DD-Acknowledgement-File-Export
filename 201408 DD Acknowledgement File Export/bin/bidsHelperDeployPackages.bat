782222ccc43b46dbb93362ff8f9ba7489e369938
pcarletonBDCH <p.carleton@battersea.org.uk>
2015-07-23 14:39:54 +0100

    1st commit

---
+++ 201408 DD Acknowledgement File Export/bin/bidsHelperDeployPackages.bat
@@ -0,0 +1,2 @@
+mkdir "\\infosphere\D$\SSIS PACKAGES\NewlyDeployedDTS\"
+xcopy "I:\201408 DD Acknowledgement File Export\201408 DD Acknowledgement File Export\FO33DDAckExport.dtsx" "\\infosphere\D$\SSIS PACKAGES\NewlyDeployedDTS\" /Y /R
